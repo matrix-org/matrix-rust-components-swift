3b7a44537f5d54f0fe899d73027e43b7f8c5c9b8
Doug <douglase@element.io>
2022-06-16 10:31:20 +0100

    Add a useLocalBinary property to Package.swift.
    Update README.md

--- Package.swift
+++ Package.swift
@@ -7,6 +7,10 @@ let checksum = "af6d006e0735edb6dcb6e17bee1776eb8c140d532abc4153647c1cb761a9af5f
 let version = "v1.0.10-alpha"
 let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 
+let useLocalBinary = false
+let binaryTarget: Target = useLocalBinary ? .binaryTarget(name: "MatrixSDKFFI", path: "MatrixSDKFFI.xcframework")
+                                          : .binaryTarget(name: "MatrixSDKFFI", url: url, checksum: checksum)
+
 let package = Package(
     name: "MatrixRustSDK",
     platforms: [
@@ -19,13 +23,7 @@ let package = Package(
             targets: ["MatrixRustSDK"]),
     ],
     targets: [
-//        .binaryTarget(
-//            name: "MatrixSDKFFI",
-//            path: "MatrixSDKFFI.xcframework"),
-        .binaryTarget(
-            name: "MatrixSDKFFI",
-            url: url,
-            checksum: checksum),
+        binaryTarget,
         /*
          * A placeholder wrapper for our binaryTarget so that Xcode will ensure this is
          * downloaded/built before trying to use it in the build process
