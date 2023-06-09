9d789df70f018a2bb042818aced00ff525599bd4
Mauro Romito <mauro.romito@element.io>
2023-06-09 13:41:46 +0200

    Revert "Bump to 1.0.74-alpha (matrix-rust-sdk ae2f834345e8b54ae6cd3f0f13bbf98190f8aee5)"
    This reverts commit 3d6f775a20852060ec0f9306af1cab628abdaccf.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let checksum = "29b49c3df3116ae63b991e3d8565e8a2cf3316fc9f3016a55e193a3502cfc64f"
-let version = "1.0.74-alpha"
+let version = "1.0.73-alpha"
 let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 
 let package = Package(
