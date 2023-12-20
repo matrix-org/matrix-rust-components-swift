2f0af88a231050947ecd288e0aa4555f5f25bc27
Mauro Romito <mauro.romito@element.io>
2023-12-20 16:55:00 +0100

    Bump to v0.0.1-recovery-state-debugging (matrix-rust-sdk/stefan/recoveryStateDebugging a730527b1a254945bb8530de2ef0cf0131a9e4c8)

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let checksum = "437e9506a4513effc8deb6c2ec42ff61e4c194616db096dab8fac5737c40ede7"
-let version = "v1.1.29"
+let checksum = "068d337ebde0890d9fbc3ea31f6d93d31c2c626b3a28ace1a17ec33d854b00d1"
+let version = "v0.0.1-recovery-state-debugging"
 let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 
 let package = Package(
