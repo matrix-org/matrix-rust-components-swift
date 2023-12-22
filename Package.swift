c0027815b6ac690837e4f437ae874e7a4b5bc2a7
Mauro Romito <mauro.romito@element.io>
2023-12-22 11:50:15 +0100

    Bump to v0.0.1-december23 (matrix-rust-sdk/mauroromito/release_elx d4b8f88e10d8fdd066d529e28c30b87af6c14c55)

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let checksum = "174db31d9bd30283d910dff26b238365cb9f477b70118578e256f7ad1e1ed8fa"
-let version = "v1.1.30"
+let checksum = "e9ef3e2bfddc4db627e0717a35e78a1d141b2c171826f968d1507863ca801e92"
+let version = "v0.0.1-december23"
 let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 
 let package = Package(
