bfb73a441c6267449517869ea708e9201e7facb0
Doug <douglase@element.io>
2024-03-28 10:17:51 +0000

    Bump to v1.1.54 (matrix-rust-sdk/main ac0bc95c253c5d46fec31378016497f433e0067d)

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let checksum = "bdca00279f81b10376b9aae67625b77c76334b8e22e87365f07b79f436be9bb0"
-let version = "v1.1.53"
+let checksum = "5a170b60f0d5e659baa1dfea43a983b9d8a17180932ce1a08e4b9c8324746573"
+let version = "v1.1.54"
 let url = "https://github.com/matrix-org/matrix-rust-components-swift/releases/download/\(version)/MatrixSDKFFI.xcframework.zip"
 
 let package = Package(
