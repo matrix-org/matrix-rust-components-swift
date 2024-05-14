3a008b0e15ca1e64acd230ff9bd3fe53b83c93d9
Doug <douglase@element.io>
2024-05-14 16:18:46 +0100

    Use the shared tools in the release script.

--- Tools/Release/Package.swift
+++ Tools/Release/Package.swift
@@ -7,8 +7,15 @@ let package = Package(
     name: "Release",
     platforms: [.macOS(.v13)],
     products: [.executable(name: "release", targets: ["Release"])],
-    dependencies: [.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/element-hq/swift-command-line-tools.git", revision: "f9695015fff3c619172e5337266c46cfe5c327d7")
+    ],
     targets: [
-        .executableTarget(name: "Release", dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")])
+        .executableTarget(name: "Release", 
+                          dependencies: [
+                            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                            .product(name: "CommandLineTools", package: "swift-command-line-tools")
+                          ])
     ]
 )
--- Tools/Release/Sources/Release.swift
+++ Tools/Release/Sources/Release.swift
@@ -1,240 +1,73 @@
 import ArgumentParser
-import CryptoKit
+import CommandLineTools
 import Foundation
 
 @main
 struct Release: AsyncParsableCommand {
-    
-    @Option(help: "The version of the library that is being released.")
+    @Option(help: "The version of the package that is being released.")
     var version: String
     
     var apiToken = (try? NetrcParser.parse(file: FileManager.default.homeDirectoryForCurrentUser.appending(component: ".netrc")))!
         .authorization(for: URL(string: "https://api.github.com")!)!
         .password
     
-    var packageRepo = "matrix-org/matrix-rust-components-swift"
+    var sourceRepo = Repository(owner: "matrix-org", name: "matrix-rust-sdk")
+    var packageRepo = Repository(owner: "matrix-org", name: "matrix-rust-components-swift")
+    
     var packageDirectory = URL(fileURLWithPath: #file)
         .deletingLastPathComponent() // Release.swift
         .deletingLastPathComponent() // Sources
         .deletingLastPathComponent() // Release
         .deletingLastPathComponent() // Tools
-    
     lazy var buildDirectory = packageDirectory
         .deletingLastPathComponent() // matrix-rust-components-swift
         .appending(component: "matrix-rust-sdk")
     
     mutating func run() async throws {
-        info("Build directory: \(buildDirectory.path())")
+        let package = Package(repository: packageRepo, directory: packageDirectory, apiToken: apiToken)
+        Zsh.defaultDirectory = package.directory
         
-        let library = try buildLibrary()
-        let (zipFileURL, checksum) = try zipBinary(at: library.directory)
+        Log.info("Build directory: \(buildDirectory.path())")
         
-        try await updatePackage(with: library, checksum: checksum)
-        try commitAndPush(with: library)
-        try await makeRelease(with: library, uploading: zipFileURL)
+        let product = try build()
+        let (zipFileURL, checksum) = try package.zipBinary(with: product)
+        
+        try await updatePackage(package, with: product, checksum: checksum)
+        try commitAndPush(with: product)
+        try await package.makeRelease(with: product, uploading: zipFileURL)
     }
     
-    mutating func buildLibrary() throws -> LibraryInfo {
-        let commitHash = try run(command: "git rev-parse HEAD", directory: buildDirectory)!.trimmingCharacters(in: .whitespacesAndNewlines)
-        let branch = try run(command: "git rev-parse --abbrev-ref HEAD", directory: buildDirectory)!.trimmingCharacters(in: .whitespacesAndNewlines)
+    mutating func build() throws -> BuildProduct {
+        let commitHash = try Zsh.run(command: "git rev-parse HEAD", directory: buildDirectory)!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let branch = try Zsh.run(command: "git rev-parse --abbrev-ref HEAD", directory: buildDirectory)!.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        info("Building \(branch) at \(commitHash)")
+        Log.info("Building \(branch) at \(commitHash)")
         
-        // unset fixes an issue where swift compilation prevents building for targets other than macOS X
-        try run(command: "unset SDKROOT && cargo xtask swift build-framework --release", directory: buildDirectory)
+        // unset fixes an issue where swift compilation prevents building for targets other than macOS
+        try Zsh.run(command: "unset SDKROOT && cargo xtask swift build-framework --release", directory: buildDirectory)
         
-        return LibraryInfo(commitHash: commitHash, branch: branch, directory: buildDirectory.appending(component: "bindings/apple/generated/"))
+        return BuildProduct(sourceRepo: sourceRepo,
+                            version: version,
+                            commitHash: commitHash,
+                            branch: branch,
+                            directory: buildDirectory.appending(component: "bindings/apple/generated/"),
+                            frameworkName: "MatrixSDKFFI.xcframework")
     }
     
-    mutating func zipBinary(at libraryDirectory: URL) throws -> (URL, String) {
-        let zipFileURL = packageDirectory.appending(component: "MatrixSDKFFI.xcframework.zip")
-        if FileManager.default.fileExists(atPath: zipFileURL.path()) {
-            info("Deleting old framework")
-            try FileManager.default.removeItem(at: zipFileURL)
-        }
-
-        info("Zipping framework")
-        try run(command: "zip -r '\(zipFileURL.path())' MatrixSDKFFI.xcframework", directory: libraryDirectory)
-        let checksum = try checksum(for: zipFileURL)
-        info("Checksum: \(checksum)")
+    func updatePackage(_ package: Package, with product: BuildProduct, checksum: String) async throws {
+        Log.info("Copying sources")
+        let source = product.directory.appending(component: "swift", directoryHint: .isDirectory)
+        let destination = package.directory.appending(component: "Sources/MatrixRustSDK", directoryHint: .isDirectory)
+        try Zsh.run(command: "rsync -a --delete '\(source.path())' '\(destination.path())'")
         
-        return (zipFileURL, checksum)
+        try await package.updateManifest(with: product, checksum: checksum)
     }
     
-    func updatePackage(with library: LibraryInfo, checksum: String) async throws {
-        info("Copying sources")
-        let source = library.directory.appending(component: "swift", directoryHint: .isDirectory)
-        let destination = packageDirectory.appending(component: "Sources/MatrixRustSDK", directoryHint: .isDirectory)
-        try run(command: "rsync -a --delete '\(source.path())' '\(destination.path())'")
-        
-        info("Updating manifest")
-        let manifestURL = packageDirectory.appending(component: "Package.swift")
-        var updatedManifest = ""
-        
-        #warning("Strips empty lines")
-        for try await line in manifestURL.lines {
-            if line.starts(with: "let version = ") {
-                updatedManifest.append("let version = \"\(version)\"")
-            } else if line.starts(with: "let checksum = ") {
-                updatedManifest.append("let checksum = \"\(checksum)\"")
-            } else {
-                updatedManifest.append(line)
-            }
-            updatedManifest.append("\n")
-        }
-        
-        try updatedManifest.write(to: manifestURL, atomically: true, encoding: .utf8)
-    }
-    
-    mutating func commitAndPush(with library: LibraryInfo) throws {
-        info("Pushing changes")
-        try run(command: "git add Package.swift")
-        try run(command: "git add Sources")
-        try run(command: "git commit -m 'Bump to version \(version) (matrix-rust-sdk/\(library.branch) \(library.commitHash))'")
-        try run(command: "git push")
-    }
-    
-    func makeRelease(with library: LibraryInfo, uploading zipFileURL: URL) async throws {
-        info("Making release")
-        let url = URL(string: "https://api.github.com/repos")!
-            .appending(path: packageRepo)
-            .appending(component: "releases")
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
-        request.addValue("Bearer \(apiToken)", forHTTPHeaderField: "Authorization")
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content")
-        
-        let body = GitHubReleaseRequest(tagName: version,
-                                        targetCommitish: "main",
-                                        name: version,
-                                        body: "https://github.com/matrix-org/matrix-rust-sdk/tree/\(library.commitHash)",
-                                        draft: false,
-                                        prerelease: false,
-                                        generateReleaseNotes: false,
-                                        makeLatest: "true")
-        
-        let encoder = JSONEncoder()
-        encoder.keyEncodingStrategy = .convertToSnakeCase
-        let bodyData = try encoder.encode(body)
-        request.httpBody = bodyData
-        
-        let (data, _) = try await URLSession.shared.data(for: request)
-        let release = try JSONDecoder().decode(GitHubRelease.self, from: data)
-        
-        info("Release created \(release.htmlURL)")
-        
-        try await uploadFramework(at: zipFileURL, to: release.uploadURL)
-    }
-    
-    func uploadFramework(at fileURL: URL, to uploadURL: URL) async throws {
-        info("Uploading framework")
-        
-        var uploadComponents = URLComponents(url: uploadURL, resolvingAgainstBaseURL: false)!
-        uploadComponents.queryItems = [URLQueryItem(name: "name", value: fileURL.lastPathComponent)]
-        
-        var request = URLRequest(url: uploadComponents.url!)
-        request.httpMethod = "POST"
-        request.addValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
-        request.addValue("Bearer \(apiToken)", forHTTPHeaderField: "Authorization")
-        request.addValue("application/zip", forHTTPHeaderField: "Content-Type")
-        
-        let (data, response) = try await URLSession.shared.upload(for: request, fromFile: fileURL)
-        
-        guard let httpResponse = response as? HTTPURLResponse else {
-            throw ReleaseError.httpResponse(-1)
-        }
-        guard httpResponse.statusCode == 201 else {
-            throw ReleaseError.httpResponse(httpResponse.statusCode)
-        }
-        
-        let upload = try JSONDecoder().decode(GitHubUploadResponse.self, from: data)
-        info("Upload finished \(upload.browserDownloadURL)")
-    }
-    
-    // MARK: Helpers
-    
-    private func info(_ message: String) {
-        print("🚀 \(message)")
-    }
-    
-    @discardableResult
-    private func run(command: String, directory: URL? = nil) throws -> String? {
-        let process = Process()
-        let outputPipe = Pipe()
-        
-        process.executableURL = URL(fileURLWithPath: "/bin/zsh")
-        process.arguments = ["-cu", command]
-        process.currentDirectoryURL = directory ?? packageDirectory
-        process.standardOutput = outputPipe
-        
-        try process.run()
-        process.waitUntilExit()
-        
-        guard process.terminationReason == .exit, process.terminationStatus == 0 else {
-            throw ReleaseError.commandFailure(command: command, directory: directory ?? packageDirectory)
-        }
-        
-        guard let outputData = try outputPipe.fileHandleForReading.readToEnd() else { return nil }
-        return String(data: outputData, encoding: .utf8)
-    }
-    
-    private func checksum(for fileURL: URL) throws -> String {
-        var hasher = SHA256()
-        let handle = try FileHandle(forReadingFrom: fileURL)
-        
-        while let bytes = try handle.read(upToCount: SHA256.blockByteCount) {
-            hasher.update(data: bytes)
-        }
-        
-        let digest = hasher.finalize()
-        return digest.map { String(format: "%02hhx", $0) }.joined()
-    }
-}
-
-struct LibraryInfo {
-    let commitHash: String
-    let branch: String
-    let directory: URL
-}
-
-enum ReleaseError: Error {
-    case commandFailure(command: String, directory: URL)
-    case httpResponse(Int)
-}
-
-// MARK: - GitHub Release https://docs.github.com/en/rest/releases/releases#create-a-release
-
-struct GitHubReleaseRequest: Encodable {
-    let tagName: String
-    let targetCommitish: String
-    let name: String
-    let body: String
-    let draft: Bool
-    let prerelease: Bool
-    let generateReleaseNotes: Bool
-    let makeLatest: String
-}
-
-struct GitHubRelease: Decodable {
-    let htmlURL: URL
-    let uploadURLString: String // Decode as a string to avoid URL percent encoding.
-    
-    var uploadURL: URL {
-        URL(string: String(uploadURLString.split(separator: "{")[0]))!
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case htmlURL = "html_url"
-        case uploadURLString = "upload_url"
-    }
-}
-
-struct GitHubUploadResponse: Decodable {
-    let browserDownloadURL: String
-    
-    enum CodingKeys: String, CodingKey {
-        case browserDownloadURL = "browser_download_url"
+    func commitAndPush(with product: BuildProduct) throws {
+        Log.info("Pushing changes")
+        try Zsh.run(command: "git add Package.swift")
+        try Zsh.run(command: "git add Sources")
+        try Zsh.run(command: "git commit -m 'Bump to version \(version) (\(product.sourceRepo.name)/\(product.branch) \(product.commitHash))'")
+        try Zsh.run(command: "git push")
     }
 }
